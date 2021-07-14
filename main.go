4871a3ef34cf244c12c292d6fd7e69e24ef29a1f
Eken Chan <maskedeken@yahoo.com>
2021-07-14 15:59:13 +0800

    add RESERVED ip list

--- main.go
+++ main.go
@@ -3,65 +3,68 @@ package main
 import (
 	"bufio"
 	"flag"
+	"os"
+
 	"github.com/maxmind/mmdbwriter"
 	"github.com/maxmind/mmdbwriter/mmdbtype"
 	log "github.com/sirupsen/logrus"
-	"os"
 )
-		//"country": mmdbtype.Map{
-		//"continent": mmdbtype.Map{
-		//"code": mmdbtype.String("AS"),
-		//"geoname_id": mmdbtype.Uint32(6255147),
-		//"names":mmdbtype.Map{"de":mmdbtype.String("Asien"),
-		//"en":mmdbtype.String("Asia"),
-		//"es":mmdbtype.String("Asia"),
-		//"fr":mmdbtype.String("Asie"),
-		//"ja":mmdbtype.String("アジア"),
-		//"pt-BR":mmdbtype.String("Ásia"),
-		//"ru":mmdbtype.String("Азия"),
-		//"zh-CN":mmdbtype.String("亚洲")},
-		//},
-		//"country": mmdbtype.Map{
-		//"geoname_id":mmdbtype.Uint32(1814991),
-		//"is_in_european_union":mmdbtype.Bool(false),
-		//"iso_code":mmdbtype.String("CN"),
-		//"names":mmdbtype.Map{
-		//"de":mmdbtype.String("China"),
-		//"en":mmdbtype.String("China"),
-		//"es":mmdbtype.String("China"),
-		//"fr":mmdbtype.String("Chine"),
-		//"ja":mmdbtype.String("中国"),
-		//"pt-BR":mmdbtype.String("China"),
-		//"ru":mmdbtype.String("Китай"),
-		//"zh-CN":mmdbtype.String("中国"),
-		//},
-		//},
-		//"registered_country": mmdbtype.Map{
-		//"geoname_id":mmdbtype.Uint32(1814991),
-		//"is_in_european_union":mmdbtype.Bool(false),
-		//"iso_code":mmdbtype.String("CN"),
-		//"names":mmdbtype.Map{
-		//"de":mmdbtype.String("China"),
-		//"en":mmdbtype.String("China"),
-		//"es":mmdbtype.String("China"),
-		//"fr":mmdbtype.String("Chine"),
-		//"ja":mmdbtype.String("中国"),
-		//"pt-BR":mmdbtype.String("China"),
-		//"ru":mmdbtype.String("Китай"),
-		//"zh-CN":mmdbtype.String("中国"),
-		//},
-		//},
-		//"traits": mmdbtype.Map{
-		//"is_anonymous_proxy": mmdbtype.Bool(false),
-		//"is_satellite_provider":mmdbtype.Bool(false),
-		//},
-		//},
+
+//"country": mmdbtype.Map{
+//"continent": mmdbtype.Map{
+//"code": mmdbtype.String("AS"),
+//"geoname_id": mmdbtype.Uint32(6255147),
+//"names":mmdbtype.Map{"de":mmdbtype.String("Asien"),
+//"en":mmdbtype.String("Asia"),
+//"es":mmdbtype.String("Asia"),
+//"fr":mmdbtype.String("Asie"),
+//"ja":mmdbtype.String("アジア"),
+//"pt-BR":mmdbtype.String("Ásia"),
+//"ru":mmdbtype.String("Азия"),
+//"zh-CN":mmdbtype.String("亚洲")},
+//},
+//"country": mmdbtype.Map{
+//"geoname_id":mmdbtype.Uint32(1814991),
+//"is_in_european_union":mmdbtype.Bool(false),
+//"iso_code":mmdbtype.String("CN"),
+//"names":mmdbtype.Map{
+//"de":mmdbtype.String("China"),
+//"en":mmdbtype.String("China"),
+//"es":mmdbtype.String("China"),
+//"fr":mmdbtype.String("Chine"),
+//"ja":mmdbtype.String("中国"),
+//"pt-BR":mmdbtype.String("China"),
+//"ru":mmdbtype.String("Китай"),
+//"zh-CN":mmdbtype.String("中国"),
+//},
+//},
+//"registered_country": mmdbtype.Map{
+//"geoname_id":mmdbtype.Uint32(1814991),
+//"is_in_european_union":mmdbtype.Bool(false),
+//"iso_code":mmdbtype.String("CN"),
+//"names":mmdbtype.Map{
+//"de":mmdbtype.String("China"),
+//"en":mmdbtype.String("China"),
+//"es":mmdbtype.String("China"),
+//"fr":mmdbtype.String("Chine"),
+//"ja":mmdbtype.String("中国"),
+//"pt-BR":mmdbtype.String("China"),
+//"ru":mmdbtype.String("Китай"),
+//"zh-CN":mmdbtype.String("中国"),
+//},
+//},
+//"traits": mmdbtype.Map{
+//"is_anonymous_proxy": mmdbtype.Bool(false),
+//"is_satellite_provider":mmdbtype.Bool(false),
+//},
+//},
 
 var (
-	srcFile string
-	dstFile string
+	srcFile      string
+	reservedFile string
+	dstFile      string
 	databaseType string
-	cnRecord = mmdbtype.Map{
+	cnRecord     = mmdbtype.Map{
 		"country": mmdbtype.Map{
 			"geoname_id":           mmdbtype.Uint32(1814991),
 			"is_in_european_union": mmdbtype.Bool(false),
@@ -78,20 +81,32 @@ var (
 			},
 		},
 	}
+	reservedRecord = mmdbtype.Map{
+		"country": mmdbtype.Map{
+			"geoname_id":           mmdbtype.Uint32(9999999),
+			"is_in_european_union": mmdbtype.Bool(false),
+			"iso_code":             mmdbtype.String("RESERVED"),
+			"names": mmdbtype.Map{
+				"en": mmdbtype.String("Reserved"),
+			},
+		},
+	}
 )
 
-func init()  {
+func init() {
 	flag.StringVar(&srcFile, "s", "ipip_cn.txt", "specify source ip list file")
+	flag.StringVar(&reservedFile, "r", "", "specify reserved ip list file")
 	flag.StringVar(&dstFile, "d", "Country.mmdb", "specify destination mmdb file")
-	flag.StringVar(&databaseType,"t", "GeoIP2-Country", "specify MaxMind database type")
+	flag.StringVar(&databaseType, "t", "GeoIP2-Country", "specify MaxMind database type")
 	flag.Parse()
 }
 
-func main()  {
+func main() {
 	writer, err := mmdbwriter.New(
 		mmdbwriter.Options{
-			DatabaseType: databaseType,
-			RecordSize:   24,
+			DatabaseType:            databaseType,
+			RecordSize:              24,
+			IncludeReservedNetworks: true,
 		},
 	)
 	if err != nil {
@@ -99,10 +114,14 @@ func main()  {
 	}
 
 	var ipTxtList []string
+	var reservedList []string
+
 	fh, err := os.Open(srcFile)
 	if err != nil {
 		log.Fatalf("fail to open %s\n", err)
 	}
+	defer fh.Close()
+
 	scanner := bufio.NewScanner(fh)
 	scanner.Split(bufio.ScanLines)
 
@@ -110,9 +129,30 @@ func main()  {
 		ipTxtList = append(ipTxtList, scanner.Text())
 	}
 
-	ipList := parseCIDRs(ipTxtList)
-	for _, ip := range ipList {
+	if len(reservedFile) != 0 {
+		fh, err := os.Open(reservedFile)
+		if err != nil {
+			log.Fatalf("fail to open %s\n", err)
+		}
+		defer fh.Close()
+
+		scanner := bufio.NewScanner(fh)
+		scanner.Split(bufio.ScanLines)
+
+		for scanner.Scan() {
+			reservedList = append(reservedList, scanner.Text())
+		}
+	}
+
+	for _, ip := range parseCIDRs(ipTxtList) {
 		err = writer.Insert(ip, cnRecord)
+		if err != nil {
+			log.Fatalf("fail to insert to writer %v\n", err)
+		}
+	}
+
+	for _, ip := range parseCIDRs(reservedList) {
+		err = writer.Insert(ip, reservedRecord)
 		if err != nil {
 			log.Fatalf("fail to insert to writer %v\n", err)
 		}
@@ -129,5 +169,3 @@ func main()  {
 	}
 
 }
-
-
--- verify/verify_ip.go
+++ verify/verify_ip.go
@@ -2,9 +2,10 @@ package main
 
 import (
 	"fmt"
-	"github.com/oschwald/geoip2-golang"
 	"log"
 	"net"
+
+	"github.com/oschwald/geoip2-golang"
 )
 
 const DATA_FILE = "Country.mmdb"
@@ -18,7 +19,7 @@ func main() {
 	// If you are using strings that may be invalid, check that ip is not nil
 
 	var list = []string{"103.200.30.143", "103.228.130.61", "216.58.200.238",
-		"103.200.30.245", "118.184.26.113", "103.200.31.172", "69.171.235.101", "123.126.55.41", "117.23.61.238"}
+		"103.200.30.245", "118.184.26.113", "103.200.31.172", "69.171.235.101", "123.126.55.41", "117.23.61.238", "192.168.1.1"}
 	// "103.200.30.143", "103.228.130.61", "216.58.200.238",
 	//	"103.200.30.245", "118.184.26.113", "103.200.31.172", "69.171.235.101"
 	// is custom ip from gfw and be clarified as HK in ipip but china in maxmind
@@ -31,13 +32,10 @@ func main() {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("IP:%s-Locale:%s\n", ipTxt,record.Country.IsoCode)
+		fmt.Printf("IP:%s-Locale:%s\n", ipTxt, record.Country.IsoCode)
 		//fmt.Printf("%d, %s, %s|\n %s, %d, %s, %v|\n %s, %d, %v, %s|\n %s, %s, %v, %v|\n ", record.Continent.GeoNameID, record.Continent.Code, record.Continent.Names,
 		//	record.Country.Names, record.Country.GeoNameID, record.Country.IsoCode, record.Country.IsInEuropeanUnion,
 		//	record.RegisteredCountry.Names, record.RegisteredCountry.GeoNameID, record.RegisteredCountry.IsInEuropeanUnion, record.RegisteredCountry.IsoCode,
 		//	record.RepresentedCountry.IsoCode, record.RepresentedCountry.Type, record.Traits.IsAnonymousProxy, record.Traits.IsSatelliteProvider)
 	}
 }
-
-
-
