1140bb1d071af772ee2779b17c9dbdb13318c8d3
Eken Chan <maskedeken@yahoo.com>
2021-07-15 14:07:35 +0800

    support custom categories

--- main.go
+++ main.go
@@ -4,86 +4,29 @@ import (
 	"bufio"
 	"flag"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/maxmind/mmdbwriter"
 	"github.com/maxmind/mmdbwriter/mmdbtype"
 	log "github.com/sirupsen/logrus"
 )
 
-//"country": mmdbtype.Map{
-//"continent": mmdbtype.Map{
-//"code": mmdbtype.String("AS"),
-//"geoname_id": mmdbtype.Uint32(6255147),
-//"names":mmdbtype.Map{"de":mmdbtype.String("Asien"),
-//"en":mmdbtype.String("Asia"),
-//"es":mmdbtype.String("Asia"),
-//"fr":mmdbtype.String("Asie"),
-//"ja":mmdbtype.String("アジア"),
-//"pt-BR":mmdbtype.String("Ásia"),
-//"ru":mmdbtype.String("Азия"),
-//"zh-CN":mmdbtype.String("亚洲")},
-//},
-//"country": mmdbtype.Map{
-//"geoname_id":mmdbtype.Uint32(1814991),
-//"is_in_european_union":mmdbtype.Bool(false),
-//"iso_code":mmdbtype.String("CN"),
-//"names":mmdbtype.Map{
-//"de":mmdbtype.String("China"),
-//"en":mmdbtype.String("China"),
-//"es":mmdbtype.String("China"),
-//"fr":mmdbtype.String("Chine"),
-//"ja":mmdbtype.String("中国"),
-//"pt-BR":mmdbtype.String("China"),
-//"ru":mmdbtype.String("Китай"),
-//"zh-CN":mmdbtype.String("中国"),
-//},
-//},
-//"registered_country": mmdbtype.Map{
-//"geoname_id":mmdbtype.Uint32(1814991),
-//"is_in_european_union":mmdbtype.Bool(false),
-//"iso_code":mmdbtype.String("CN"),
-//"names":mmdbtype.Map{
-//"de":mmdbtype.String("China"),
-//"en":mmdbtype.String("China"),
-//"es":mmdbtype.String("China"),
-//"fr":mmdbtype.String("Chine"),
-//"ja":mmdbtype.String("中国"),
-//"pt-BR":mmdbtype.String("China"),
-//"ru":mmdbtype.String("Китай"),
-//"zh-CN":mmdbtype.String("中国"),
-//},
-//},
-//"traits": mmdbtype.Map{
-//"is_anonymous_proxy": mmdbtype.Bool(false),
-//"is_satellite_provider":mmdbtype.Bool(false),
-//},
-//},
-
 var (
-	srcFile      string
-	reservedFile string
-	dstFile      string
-	databaseType string
-	cnRecord     = mmdbtype.Map{
+	dataPath   string
+	outputName string
+	outputDir  string
+	cnRecord   = mmdbtype.Map{
 		"country": mmdbtype.Map{
-			"geoname_id":           mmdbtype.Uint32(1814991),
 			"is_in_european_union": mmdbtype.Bool(false),
 			"iso_code":             mmdbtype.String("CN"),
 			"names": mmdbtype.Map{
-				"de":    mmdbtype.String("China"),
-				"en":    mmdbtype.String("China"),
-				"es":    mmdbtype.String("China"),
-				"fr":    mmdbtype.String("Chine"),
-				"ja":    mmdbtype.String("中国"),
-				"pt-BR": mmdbtype.String("China"),
-				"ru":    mmdbtype.String("Китай"),
-				"zh-CN": mmdbtype.String("中国"),
+				"en": mmdbtype.String("China"),
 			},
 		},
 	}
 	reservedRecord = mmdbtype.Map{
 		"country": mmdbtype.Map{
-			"geoname_id":           mmdbtype.Uint32(9999999),
 			"is_in_european_union": mmdbtype.Bool(false),
 			"iso_code":             mmdbtype.String("RESERVED"),
 			"names": mmdbtype.Map{
@@ -94,31 +37,18 @@ var (
 )
 
 func init() {
-	flag.StringVar(&srcFile, "s", "ipip_cn.txt", "specify source ip list file")
-	flag.StringVar(&reservedFile, "r", "", "specify reserved ip list file")
-	flag.StringVar(&dstFile, "d", "Country.mmdb", "specify destination mmdb file")
-	flag.StringVar(&databaseType, "t", "GeoIP2-Country", "specify MaxMind database type")
+	flag.StringVar(&dataPath, "datapath", "./data", "specify directory which contains ip list files")
+	flag.StringVar(&outputName, "outputname", "Country.mmdb", "specify destination mmdb file")
+	flag.StringVar(&outputDir, "outputdir", "./", "Directory to place all generated files")
 	flag.Parse()
 }
 
-func main() {
-	writer, err := mmdbwriter.New(
-		mmdbwriter.Options{
-			DatabaseType:            databaseType,
-			RecordSize:              24,
-			IncludeReservedNetworks: true,
-		},
-	)
-	if err != nil {
-		log.Fatalf("fail to new writer %v\n", err)
-	}
-
+func load(path string) ([]string, error) {
 	var ipTxtList []string
-	var reservedList []string
 
-	fh, err := os.Open(srcFile)
+	fh, err := os.Open(path)
 	if err != nil {
-		log.Fatalf("fail to open %s\n", err)
+		return nil, err
 	}
 	defer fh.Close()
 
@@ -128,44 +58,78 @@ func main() {
 	for scanner.Scan() {
 		ipTxtList = append(ipTxtList, scanner.Text())
 	}
+	return ipTxtList, nil
+}
 
-	if len(reservedFile) != 0 {
-		fh, err := os.Open(reservedFile)
-		if err != nil {
-			log.Fatalf("fail to open %s\n", err)
-		}
-		defer fh.Close()
-
-		scanner := bufio.NewScanner(fh)
-		scanner.Split(bufio.ScanLines)
-
-		for scanner.Scan() {
-			reservedList = append(reservedList, scanner.Text())
-		}
-	}
-
-	for _, ip := range parseCIDRs(ipTxtList) {
-		err = writer.Insert(ip, cnRecord)
-		if err != nil {
-			log.Fatalf("fail to insert to writer %v\n", err)
-		}
-	}
-
-	for _, ip := range parseCIDRs(reservedList) {
-		err = writer.Insert(ip, reservedRecord)
-		if err != nil {
-			log.Fatalf("fail to insert to writer %v\n", err)
-		}
-	}
-
-	outFh, err := os.Create(dstFile)
+func main() {
+	writer, err := mmdbwriter.New(
+		mmdbwriter.Options{
+			DatabaseType:            "GeoIP2-Country",
+			RecordSize:              24,
+			IncludeReservedNetworks: true,
+		},
+	)
 	if err != nil {
-		log.Fatalf("fail to create output file %v\n", err)
+		log.Fatalf("fail to new writer: %v", err)
+	}
+
+	// Create output directory if not exist
+	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
+		if mkErr := os.MkdirAll(outputDir, 0755); mkErr != nil {
+			log.Fatalf("fail to create output directory: %v", err)
+		}
+	}
+
+	ref := make(map[string][]string)
+	err = filepath.Walk(dataPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+
+		list, err := load(path)
+		if err != nil {
+			return err
+		}
+
+		name := strings.ToUpper(filepath.Base(path))
+		ref[name] = list
+		return nil
+	})
+
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	for refName, list := range ref {
+		rec := mmdbtype.Map{
+			"country": mmdbtype.Map{
+				"is_in_european_union": mmdbtype.Bool(false),
+				"iso_code":             mmdbtype.String(refName),
+				"names": mmdbtype.Map{
+					"en": mmdbtype.String(refName),
+				},
+			},
+		}
+
+		for _, ip := range parseCIDRs(list) {
+			err = writer.Insert(ip, rec)
+			if err != nil {
+				log.Fatalf("fail to insert to writer: %v", err)
+			}
+		}
+	}
+
+	outFh, err := os.Create(filepath.Join(outputDir, outputName))
+	if err != nil {
+		log.Fatalf("fail to create output file: %v", err)
 	}
 
 	_, err = writer.WriteTo(outFh)
 	if err != nil {
-		log.Fatalf("fail to write to file %v\n", err)
+		log.Fatalf("fail to write to file: %v", err)
 	}
 
 }
